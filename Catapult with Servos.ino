4173014f0bfe67b95b169d1509a6311c2a5f5eea
ProficiaTech <59269244+ProficiaTech@users.noreply.github.com>
2019-12-30 11:39:06 -0800

    Add files via upload

---
+++ Catapult with Servos.ino
@@ -0,0 +1,54 @@
+#include <Servo.h>
+Servo myservo; // create servo object to control a servo
+Servo myservo2;
+Servo myservo3;
+int potpin = 0; // analog pin used to connect the potentiometer
+int val; // variable to read the value from the analog pin
+int potpin2 = 1;
+int val2;
+const int buttonPin= 2 ;
+const int ledPin = 13;
+int buttonState = 0;
+int pos = 0;
+int pos1 =200;
+void setup()
+{
+ myservo.attach(9); // attaches the servo on pin 9 to the servo object
+ myservo2.attach(10);
+ myservo3.attach(11);
+ pinMode(ledPin, OUTPUT);
+ pinMode(buttonPin,INPUT);
+ Serial.begin(9600);
+}
+void loop()
+{
+ val = analogRead(potpin);   // reads the value of the
+ val2 = analogRead(potpin2);
+          // potentiometer (value between
+          // 0 and 1023)
+ val = map(val, 0, 1023, 0, 179);  // scale it to use it with
+ val2 = map(val2, 0, 1023, 0, 179);
+          // the servo (value between 0 and
+          // 180)
+ myservo.write(val);     // sets the servo position according
+ myservo2.write(val2);
+          // to the scaled value
+ delay(15);       // waits for the servo to get there
+ int buttonState = digitalRead(buttonPin);
+ Serial.println(buttonState);
+ delay(1);
+ digitalRead(buttonPin);
+  if (buttonState == HIGH)
+    {
+      digitalWrite(ledPin, HIGH);
+      myservo3.write(pos);
+      delay(10);
+    }
+  else
+    {
+      digitalWrite(ledPin, LOW);
+      myservo3.write(pos1);
+      delay(15);
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Catapult-w-Servos
+This repo contains files specifically  crated for the catapult with servo motors.
